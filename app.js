dc9a4a66d23baa1cf3b16bdf067e1c0cd2990d4c
Anurag Kasudhan <114092146+Kasudhananurag@users.noreply.github.com>
2022-11-30 12:59:57 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bp = require('body-parser');
+const path = require('path');
+
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop")
+
+const app = express();
+
+app.use(bp.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'public')))
+
+app.use('/admin',adminData.router);
+app.use(shopRoutes);
+
+app.use((req,res) => {
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
+})
+
+app.listen(3000);
